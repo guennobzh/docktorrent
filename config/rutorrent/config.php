a897703c349eb1e78fd48c2130486ff74fec0c3d
guenno <guenno@laposte.net>
2020-04-06 18:27:51 +0200

    modif mini pour que ca build et deplacement de rpc.socket vers /rtorrent

--- config/rutorrent/config.php
+++ config/rutorrent/config.php
@@ -27,14 +27,14 @@
 	$topDirectory = '/rtorrent/';		// Upper available directory. Absolute path with trail slash.
 	$forbidUserSettings = false;
 
-	# $scgi_port = 5000;
-	# $scgi_host = "127.0.0.1";
+	#$scgi_port = 5000;
+	#$scgi_host = "127.0.0.1";
 
 	// For web->rtorrent link through unix domain socket 
 	// (scgi_local in rtorrent conf file), change variables 
 	// above to something like this:
 	$scgi_port = 0;
-	$scgi_host = "unix:///tmp/rpc.socket";
+	$scgi_host = "unix:///rtorrent/rpc.socket";
 
 	$XMLRPCMountPoint = "/RPC2";		// DO NOT DELETE THIS LINE!!! DO NOT COMMENT THIS LINE!!!
 
